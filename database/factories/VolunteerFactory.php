b99fb21b728029c72106c9fcc755c92e3d675ddd
adrianmulyawan <adrianmulyawan666@gmail.com>
2024-01-24 22:01:15 +0700

    insert new dummy data

---
+++ database/factories/VolunteerFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Volunteer>
+ */
+class VolunteerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'project_id' => 3,
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'phone_number' => fake()->phoneNumber()
+        ];
+    }
+}
