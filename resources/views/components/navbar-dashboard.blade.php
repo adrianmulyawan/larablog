f66f8172332323a8bb7a3196f1688ab02f94c9cf
adrianmulyawan <adrianmulyawan666@gmail.com>
2024-01-25 20:16:12 +0700

    edit navbar dashboard

--- resources/views/components/navbar-dashboard.blade.php
+++ resources/views/components/navbar-dashboard.blade.php
@@ -1,6 +1,6 @@
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
     <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3" href="index.html">Dashboard Admin</a>
+    <a class="navbar-brand ps-3" href="{{ route('dashboard') }}">Dashboard Admin</a>
     <!-- Sidebar Toggle-->
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
             class="fas fa-bars"></i></button>
@@ -14,6 +14,7 @@
             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
                 data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="{{ route('home') }}">Halaman Utama</a></li>
                 <li><a class="dropdown-item" href="{{ route('setting.index') }}">Pengaturan Akun</a></li>
                 <li><a class="dropdown-item" href="{{ route('password.index') }}">Ubah Password</a></li>
                 <li>
